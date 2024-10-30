1e4ca4db7ddb8164f06e38c9b362b9581b9445e5
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-30 13:15:00 +0530

    Initial Commit

---
+++ Cat_Answer/Cat_AnswerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_AnswerApp.swift
+//  Cat_Answer
+//
+//  Created by Thushini Abeysuriya on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct Cat_AnswerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
